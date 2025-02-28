38bed671e380b28733643e3f472e2af1d092b106
Larissa Almeida <oa.larissa@hotmail.com>
2025-02-28 09:58:38 +0100

    Lab updated

--- src/index.js
+++ src/index.js
@@ -7,53 +7,44 @@ const toastElement = document.getElementById('toast')
 const closeToastBtn = document.getElementById('close-toast')
 const toastMessages = document.getElementById('toast-message')
 
-
 // ITERATION 1: Add event listener to the start button
 startBtn.addEventListener("click", () => {
-  console.log("Start button clicked!");
   startCountdown();
 });
 
 closeToastBtn.addEventListener("click", () => {
   toastElement.classList.remove('show')
-  remainingTime = DURATION;
   time.innerText = remainingTime;
 });
 
-
-
-
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
-
-const interval = setInterval(() => {  
+startBtn.style.display = "none";
+timer = setInterval(() => {  
   remainingTime -= 1; 
   time.innerText = remainingTime;
-  if(remainingTime === DURATION - 1) {
+  if(remainingTime === DURATION -1) {
     showToast("⏰ Final countdown! ⏰")
   } else if (remainingTime === 5) {
     showToast("Start the engines! 💥")
   }
   else if (remainingTime === 0) {
-    clearInterval(interval)
+    clearInterval(timer)
     showToast()
+    startBtn.style.display = "block";
+    remainingTime = DURATION;
+    time.innerText = remainingTime;
   }
 
 }, 1000);
 }
 
-
-
 // ITERATION 3: Show Toast
 function showToast(message) {
-  toastMessages.innerText = message || "Lift off! 🚀" 
-toastElement.classList.add('show')
+   toastMessages.innerText = message || "Lift off! 🚀" 
+   toastElement.classList.add('show')
 setTimeout(() => {
-  toastElement.classList.remove('show')
+   toastElement.classList.remove('show')
 }, 3000)
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON // BONUS: ITERATION 5: MORE TOASTS MESSAGES
-
 }
 
